24787888fee7ead2dc0d12824db89c5fbe8714f5
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-02-27 07:37:38 +0100

    Update match standings after match finish
    Update standings for home and away teams after match completion.

--- backend/src/engine/matchSimulator.js
+++ backend/src/engine/matchSimulator.js
@@ -57,6 +57,35 @@ async function simulateMatches() {
   upcomingMatch.status = "finished";
 
   await upcomingMatch.save();
+  const homeStanding = await Standing.findOne({ team: upcomingMatch.homeTeam._id });
+const awayStanding = await Standing.findOne({ team: upcomingMatch.awayTeam._id });
+
+homeStanding.played += 1;
+awayStanding.played += 1;
+
+homeStanding.goalsFor += homeGoals;
+homeStanding.goalsAgainst += awayGoals;
+
+awayStanding.goalsFor += awayGoals;
+awayStanding.goalsAgainst += homeGoals;
+
+if (homeGoals > awayGoals) {
+  homeStanding.wins += 1;
+  homeStanding.points += 3;
+  awayStanding.losses += 1;
+} else if (awayGoals > homeGoals) {
+  awayStanding.wins += 1;
+  awayStanding.points += 3;
+  homeStanding.losses += 1;
+} else {
+  homeStanding.draws += 1;
+  awayStanding.draws += 1;
+  homeStanding.points += 1;
+  awayStanding.points += 1;
+}
+
+await homeStanding.save();
+await awayStanding.save();
 
   let result = "draw";
   if (homeGoals > awayGoals) result = "home";
