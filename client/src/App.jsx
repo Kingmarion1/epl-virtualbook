80d5bda6917b14ce8bb450c71902068edafdf83e
Prominent <119831389+Kingmarion1@users.noreply.github.com>
2026-02-27 17:56:45 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Matches from "./pages/Matches";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Leaderboard from "./pages/Leaderboard";
 import Standings from "./pages/Standings";
 
